44328d1118e4e902f7e4c651f2556c8cf974b171
Your Name <ti.rafaelteles@gmail.com>
2022-07-16 19:16:09 -0300

    lista_dados

---
+++ lista_dados.php
@@ -0,0 +1,13 @@
+<?php
+
+$Idade_list = [10, 20, 30, 40, 50, 60];         /* UM ARRAY É FORMADO POR [] E SUA POSICAO COMECA NO ZERO(0) */
+
+$Uma_Idade = $Idade_list[1];                    
+
+echo "$Uma_Idade";                              /* EXIBINDO A POSICAO DOIS DO ARRAY */
+
+
+/* EXEMPLO DE ARRAY NO PHP */
+
+
+

49a8cc4eb855214d9122f6c37e0de7dd31542a4b
Your Name <ti.rafaelteles@gmail.com>
2022-07-16 20:27:17 -0300

    loop_list

---
+++ loop_lista.php
@@ -0,0 +1,9 @@
+<?php
+
+$Idade_list = [10, 20, 30, 40, 50, 60, 70, 80];
+
+for($i = 0; $i < count($Idade_list); $i++){             /* A FUNÇÃO DO COUNT É CONTAR A QUANTIDADE DE ELEMENTOS NO ARRAY */
+    echo "$Idade_list[$i]" . PHP_EOL;
+}
+
+/* EXEMPLO: DE COMO LISTAR UM ARRAY UTILIZANDO O LACO DE REPETICAO */

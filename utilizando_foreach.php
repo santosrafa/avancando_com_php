86400c4caf7769120f953818f413b9754ff6ff6b
Your Name <ti.rafaelteles@gmail.com>
2022-07-17 13:00:45 -0300

    utilizando_foreach

---
+++ utilizando_foreach.php
@@ -0,0 +1,26 @@
+<?php
+
+/* FOREACH SIGNIFICA PARA CADA */
+/* NAO PRECISAMOS SABER O INDICE PARA UTILIZAR O FOREACH */
+
+$Contas_correntes = [
+    12389876578 => [
+        'titular' => 'Rafael',
+        'saldo' => 200
+    ],
+    12389876543 => [
+        'titular' => 'Maria',
+        'saldo' => 200
+    ],
+    98076853421 =>[
+
+    'Vinicius',
+    'saldo' => 300
+    ],
+
+];
+
+foreach ($Contas_correntes as $cpf => $conta){          /* ESTAMOS CHAMANDO O INDICE DO ARRAY DE $CPF */
+    echo $cpf . PHP_EOL;                                /* O AS DA UM APELIDO A VARIAVEL FICANDO MAIS FACIL CHAMAR OS DADOS */
+}
+
